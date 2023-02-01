68e4d57fc597a07cbe55bca8dc94e9e991bcd1a2
yomiyou <yomiyoubelay18@gmail.com>
2023-02-01 15:55:15 +0300

    color detection project

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,17 @@
+
+
+# function to calculate minimum distance from all colors and get the most matching color
+def get_color_name(R, G, B):
+    minimum = 10000
+    for i in range(len(csv)):
+        d = abs(R - int(csv.loc[i, "R"])) + abs(G - int(csv.loc[i, "G"])) + abs(B - int(csv.loc[i, "B"]))
+        if d <= minimum:
+            minimum = d
+            cname = csv.loc[i, "color_name"]
+    return cname
+
+
+# function to get x,y coordinates of mouse double click
+def draw_function(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        global b, g, r, x_pos, y_pos, clicked
